3353c2131d04da60652f1cbc9a7ea3dbc2bec86b
youngsterxyf <sas.198708@gmail.com>
2012-07-05 11:40:11 +0800

    修改配置文件的注释

--- version2.0/index/config.py
+++ version2.0/index/config.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
-# Berkely数据库文件位置
+# Berkely数据库文件位置，请自行决定存放位置
 dbFile =  "/home/dbFile.db"
 
-# 需要监控的目录列表
+# 需要监控的目录列表。可添加多个
 listenDirs = ['/home', '/etc']
--- version2.0/search/cgi-bin/cgiConfig.py
+++ version2.0/search/cgi-bin/cgiConfig.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
-#Berkely DB数据库的位置
+#Berkely DB数据库的位置。请按照自己的需求自行设置
 dbFile = '/home/dbFile.db'
 
-#根目录到URL的映射
-rootMap = {}
+#ftp用户根目录到URL的映射。请根据ftp的目录以及ftp服务的ip或域名自行设置
+rootMap = {'/home/ftp/public': 'ftp://127.0.0.1', '/home/ftp/private': 'ftp://127.0.0.1'}
