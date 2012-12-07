5050c182c71e8fc48a0756216fd384a78f30b35c
youngsterxyf <sas.198708@gmail.com>
2012-12-07 16:39:34 +0800

    logging the info

--- version2.0/index/config.py
+++ version2.0/index/config.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 # Berkely数据库文件位置，请自行决定存放位置
-dbFile =  "/home/dbFile.db"
+dbFile =  "/home/xiayf/dbFile.db"
 
 # 需要监控的目录列表。可添加多个
-listenDirs = ['/home', '/etc']
+listenDirs = ['/home/xiayf/Github']
--- version2.0/index/ftpIndexer.py
+++ version2.0/index/ftpIndexer.py
@@ -5,26 +5,30 @@ import os
 import sys
 import bsddb
 import pyinotify
+import logging
 import config
 
-def listDir(rootPath, dbFile):
+LOG_FILEANME = "index.log"
+logging.basicConfig(filename=LOG_FILEANME, level=logging.INFO)
+
+def list_dir(rootPath, dbFile):
 
     db = bsddb.btopen(dbFile,'w')
 
     for root, dirs, files in os.walk(rootPath):
-        #print root
         for directory in dirs:
             if not root.endswith('/'):
                 root += '/'
             db[root+directory] = directory
+            logging.debug(root+directory)
         for fi in files:
             if not root.endswith('/'):
                 root += '/'
             db[root+fi] = fi
-
+            logging.debug(root+fi)
     db.close()
 
-class processHandler(pyinotify.ProcessEvent):
+class process_handler(pyinotify.ProcessEvent):
     def __init__(self, dbFile):
         pathParts = dbFile.split('/')
         self.dbName = pathParts[-1]
@@ -39,25 +43,25 @@ class processHandler(pyinotify.ProcessEvent):
 
         self.dbConnect.put(event.path + '/' + event.name, event.name)
         self.dbConnect.sync()
-        print 'Create ' + event.path + '/' + event.name
+        logging.info('Create ' + event.path + '/' + event.name)
 
     def process_IN_DELETE(self, event):
         if self.dbConnect.exists(event.path + '/' + event.name):
             self.dbConnect.delete(event.path + '/' + event.name)
             self.dbConnect.sync()
-        print 'Delete ' + event.path + '/' + event.name
+        logging.info('Delete ' + event.path + '/' + event.name)
 
     def process_IN_MOVED_FROM(self, event):
         if self.dbConnect.exists(event.path + '/' + event.name):
             self.dbConnect.delete(event.path + '/' + event.name)
             self.dbConnect.sync()
-        print event.name, 'move from', event.path
+        logging.info(event.name + ' move from ' + event.path)
 
     def process_IN_MOVED_TO(self, event):
 
         self.dbConnect.put(event.path + '/' + event.name, event.name)
         self.dbConnect.sync()
-        print event.name, 'move to', event.path
+        logging.info(event.name + ' move to ' + event.path)
 
 if __name__ == '__main__':
     db = 'dbFile.db'
@@ -71,13 +75,17 @@ if __name__ == '__main__':
             if os.path.exists(dbFile):
                 os.remove(dbFile)
             for directory in config.listenDirs:
-                listDir(directory, dbFile)
+                list_dir(directory, dbFile)
 
-    ph = processHandler(dbFile)
+    ph = process_handler(dbFile)
     wm = pyinotify.WatchManager()
     notifier = pyinotify.Notifier(wm, default_proc_fun=ph)
     mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
     for directory in config.listenDirs:
         wm.add_watch(directory, mask, rec=True, auto_add=True)
-
+    '''由于内核对文件系统的实时监听功能有文件/目录数目配额的，默认貌似有点小(8192)，
+    当需要监听的ftp目录文件比较多时，监听在初始化时就会抛出Error，只要将配额适当改得大些就没问题了。
+    type sysctl -n fs.inotify.max_user_watches to read your current limit 
+    and type sysctl -n -w fs.inotify.max_user_watches=16384 to modify (increase) it.
+    '''
     notifier.loop();
