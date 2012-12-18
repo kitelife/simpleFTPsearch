69ed52c6f2038bbc85aa5e34d572ecc3168454f8
youngsterxyf <sas.198708@gmail.com>
2012-12-18 12:18:45 +0800

    update result page

--- version2.0/search/cgi-bin/result.py
+++ version2.0/search/cgi-bin/result.py
@@ -10,11 +10,46 @@ LOG_FILEANME = "search.log"
 logging.basicConfig(filename=LOG_FILEANME, level=logging.INFO)
 logging.info(os.environ['SERVER_NAME'])
 
+contentTemplate = "<html>%s%s</html>"
+headerTemplate = '''<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />%s</head>'''
+bodyTemplate = r'''
+<body>
+    <div class="resultbody">
+        <div id="search_form">
+            %s
+        </div>
+        <div id="abstract_part">
+            %s
+        </div>
+        <div id="result_items">
+            %s
+        </div>
+    </div>
+</body>
+'''
+
+googlewebfonts = "<link href='http://fonts.googleapis.com/css?family=Bitter:400,700,400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>"
+cssFile = r'''
+<link rel="stylesheet" href="../css/bootstrap/bootstrap.min.css" type="text/css">
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+'''
+
+abstract = ''
+items = ''
+
+search_form = r'''
+<form action="/cgi-bin/result.py" method="post" id="result_page_search_form">
+    <div class="search_form_inner">
+    <input name="searchterm" type="text" class="input_search">
+    <button type="submit" class="btn btn-primary">搜 索</button>
+    </div>
+</form>
+'''
+
 form = cgi.FieldStorage()
 keyWordValue = form.getvalue('searchterm')
-
 if keyWordValue == None:
-    content = "<h3>你的输入不能为空哦...</h3>"
+    abstract = "<div class='abstract'><h3>你的输入不能为空哦...</h3></div>"
 else:
     db = bsddb.btopen(cgiConfig.dbFile, 'r')
     countAll = 0
@@ -27,28 +62,23 @@ else:
             countResult += 1
     db.close()
 
-    contentTemplate = "<html>%s%s</html>"
-    headerTemplate = '''<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />%s</head>'''
-    bodyTemplate = "<body>%s%s</body>"
-
     abstract = "<div class='abstract'><h3>Result Num: %s</h3></div>" %countResult
     items = "<div class='results'>"
 
     index = 1
     for key, value in resultDict.iteritems():
-	   for root_path in cgiConfig.root_list:
-	       if root_path in key:
-		      key = key.replace(root_path, "ftp://" + os.environ['SERVER_NAME'])
-        items += "<p>%d.%s</p>" %(index, value)
-        items += "<p><a href='%s' target='_blank'>%s</a></p>" %(key, key)
+        for root_path in cgiConfig.root_list:
+            if root_path in key:
+                key = key.replace(root_path, "ftp://" + os.environ['SERVER_NAME'])
+        items += "<div class='item'><p>%d. %s</p>" %(index, value)
+        items += "<p><a href='%s' target='_blank'>%s</a></p></div>" %(key, key)
         index += 1
     items += "</div>"
-    googlewebfonts = "<link href='http://fonts.googleapis.com/css?family=Bitter:400,700,400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>"
-    cssFile = "<link href='../css/style.css' rel='stylesheet' type='text/css'>"
-    headString = googlewebfonts + cssFile
-    header = headerTemplate % (headString)
-    body = bodyTemplate % (abstract, items)
-    content = contentTemplate % (header, body)
+
+headString = googlewebfonts + cssFile
+header = headerTemplate % (headString)
+body = bodyTemplate % (search_form, abstract, items)
+content = contentTemplate % (header, body)
 
 print 'Content-Type: text/html;'
 print
